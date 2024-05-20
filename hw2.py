0768b37d9dbb2215f240b7b7518e2808e4ea8830
TatyanaSerK <163410714+TatyanaSerK@users.noreply.github.com>
2024-05-20 13:08:12 +0300

    Add files via upload
    Практическое задание по теме "Переменные."

---
+++ hw2.py
@@ -0,0 +1,7 @@
+home_work = 12
+total_time = 1.5
+name_course = 'Python'
+time_one_hw = total_time/home_work
+
+print(f"Курс: {name_course}, всего задач: {home_work}, "
+      f"затрачено часов: {total_time}, среднее время выполнения: {time_one_hw}")
