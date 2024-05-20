c78e3c6ee0b0b14880ca034b55b9b5843082b441
TatyanaSerK <163410714+TatyanaSerK@users.noreply.github.com>
2024-05-20 13:20:55 +0300

    Add files via upload
    Практическая работа по уроку "Организация программ и методы строк.

---
+++ hw4.py
@@ -0,0 +1,7 @@
+my_string = input('')                            #произвольный текст
+print(len(my_string))                            #длина строки
+print(my_string.upper())                         #верхний регистр
+print(my_string.lower())                         #нижний регистр
+print(my_string.replace(" ",""))     #убрать пробелы
+print(my_string[0])                              #первый символ
+print(my_string[-1])                             #последний символ
